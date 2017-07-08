053f349c24f9b514cf061c2e28c91f9b81002060
Christopher Harrop <bannastre@gmail.com>
2017-07-08 21:27:29 +0100

    Confirm an order has been placed

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,13 +5,23 @@ describe Takeaway do
   subject(:takeaway) { described_class.new(Menu.new({ :test_item_1 => 0.01, :test_item_2 => 0.08 })) }
   let(:menu) { double(:menu) }
 
+  it 'can show you a menu' do
+    allow(menu).to receive(:view).and_return({ :test_item_1 => 0.01, :test_item_2 => 0.08 })
+    expect(takeaway.view_menu).to eq(menu.view)
+  end
+
   it 'can place an order' do
     expect(takeaway.add_to_order(:test_item_1, 2)).to eq(0.02)
   end
 
+  before(:each) { takeaway.add_to_order(:test_item_1, 2) }
+
   it 'can add items to the order' do
-    takeaway.add_to_order(:test_item_1, 2)
     expect(takeaway.add_to_order(:test_item_2, 1)).to eq(0.1)
+  end
+
+  it 'can confirm an order has been placed' do
+    expect{ takeaway.place_order }.to change{ takeaway.view_orders.length }.by(1)
   end
 
 end
--- src/takeaway.rb
+++ src/takeaway.rb
@@ -5,12 +5,24 @@ class Takeaway
 
   def initialize(menu = Menu.new())
     @menu = menu
+    @orders = {}
+  end
+
+  def view_menu
+    @menu.view
+  end
+
+  def view_orders
+    @orders
   end
 
   def add_to_order(dish, quantity)
-    @order ||= Order.new(@menu)
-    @order.add_to_basket(dish, quantity)
-    @order.total
+    @current_order ||= Order.new(@menu)
+    @current_order.add_to_basket(dish, quantity)
+    @current_order.total
   end
 
+  def place_order
+    @orders[@current_order.view_basket] = @current_order.total
+  end
 end
