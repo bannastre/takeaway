e32e0c24da45b9114754941037829ae154035528
Christopher Harrop <bannastre@gmail.com>
2017-07-08 20:32:40 +0100

    refactor Order class, remove test menu from init

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,11 +2,16 @@ require './src/takeaway.rb'
 
 describe Takeaway do
 
-  subject(:takeaway) { described_class.new }
-  let(:menu) { double(:menu, :items => { :test_item_1 => 0.01 }) }
+  subject(:takeaway) { described_class.new(Menu.new({ :test_item_1 => 0.01, :test_item_2 => 0.08 })) }
+  let(:menu) { double(:menu) }
 
   it 'can place an order' do
-    expect(takeaway.new_order(:test_item_1, 2)).to eq(0.02)
+    expect(takeaway.add_to_order(:test_item_1, 2)).to eq(0.02)
+  end
+
+  it 'can add items to the order' do
+    takeaway.add_to_order(:test_item_1, 2)
+    expect(takeaway.add_to_order(:test_item_2, 1)).to eq(0.1)
   end
 
 end
--- src/takeaway.rb
+++ src/takeaway.rb
@@ -3,12 +3,12 @@ require_relative 'order'
 
 class Takeaway
 
-  def initialize(menu = Menu.new({ :test_item_1 => 0.01 }))
+  def initialize(menu = Menu.new())
     @menu = menu
   end
 
-  def new_order(dish, quantity)
-    @order = Order.new(@menu)
+  def add_to_order(dish, quantity)
+    @order ||= Order.new(@menu)
     @order.add_to_basket(dish, quantity)
     @order.total
   end
